7c0bfa9828cfe86dd2f3a8d5fc5fe264d17ec19f
huyingjie <2020173400@qq.com>
2019-08-22 17:28:03 +0800

    项目一期

---
+++ src/components/test.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div id="app-3">
+    <h1>{{ msg }}</h1>
+<!--    <p v-if="seen">现在你看到我了</p>-->
+  </div>
+
+</template>
+
+
+
+<script>
+
+
+  export default {
+    name: 'app-3',
+    data() {
+      return {
+        seen: true,
+        msg: "我的第一个"
+      }
+    }
+  }
+
+
+
+</script>
+
